07356fb09062d3ee33ad1a9a9a1f0b2490c0b303
Mugdha Deshmukh <90236319+Deshmukh20@users.noreply.github.com>
2024-06-09 16:45:56 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
 
 # Define a function for prediction
 def predict_class(input_data):
